d3fca2bc4ac910a511eb35ea436f27c6c9b50dc4
caiyueliang <393900414@qq.com>
2018-07-26 15:32:07 +0800

    update

--- model_resnet_torch.py
+++ model_resnet_torch.py
@@ -1,10 +1,11 @@
+# coding=utf-8
 from torch import nn
-import torch as t
+import torch
 import torch.nn.functional as F
+from torch.autograd import Variable
 
 
 class ResidualBlock(nn.Module):
-
     def __init__(self, in_channel, out_channel, stride=1, shortcut=None):
         super(ResidualBlock, self).__init__()
         self.left = nn.Sequential(
@@ -25,6 +26,55 @@ class ResidualBlock(nn.Module):
 
 
 class ResNet(nn.Module):
-
-    def __init__(self, num_classes=1000):
+    def __init__(self, num_classes=8):
         super(ResNet, self).__init__()
+        # 前几层图像转换
+        self.pre = nn.Sequential(
+            nn.Conv2d(3, 64, 7, 2, 3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2, 1)
+        )
+
+        # 重复的layer，分别有3,4,6,3个residual block
+        self.layer1 = self._make_layer(64, 128, 3)
+        self.layer2 = self._make_layer(128, 256, 4, stride=2)
+        self.layer3 = self._make_layer(256, 512, 6, stride=2)
+        self.layer4 = self._make_layer(512, 512, 3, stride=2)
+
+        # 分类用的全连接（这边不是分类）
+        self.fc = nn.Linear(512, num_classes)
+
+    def _make_layer(self, in_channel, out_channel, block_num, stride=1):
+        shortcut = nn.Sequential(
+            nn.Conv2d(in_channel, out_channel, 1, stride, bias=False),
+            nn.BatchNorm2d(out_channel)
+        )
+
+        layers = []
+        layers.append(ResidualBlock(in_channel, out_channel, stride, shortcut))
+
+        for ii in range(1, block_num):
+            layers.append(ResidualBlock(out_channel, out_channel))
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.pre(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = F.avg_pool2d(x, 7)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        return x
+
+
+if __name__ == '__main__':
+    model = ResNet()
+    input = Variable(torch.randn(8, 3, 224, 224))
+    print (input.size())
+    o = model(input)
+    print (o.size())
