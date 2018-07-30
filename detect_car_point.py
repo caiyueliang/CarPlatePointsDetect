8f8aa4de6761c4f3c3e2c1411c406711940ddefb
caiyueliang <393900414@qq.com>
2018-07-30 11:00:40 +0800

    update

--- detect_car_point.py
+++ detect_car_point.py
@@ -4,15 +4,15 @@ import model_resnet_torch
 from torchvision import models
 
 if __name__ == '__main__':
-    train_path = '/home/caiyueliang/deeplearning/CarPlatePointsDetect/Data/car_plate_train'
-    test_path = '/home/caiyueliang/deeplearning/CarPlatePointsDetect/Data/car_plate_test'
+    train_path = './Data/car_plate_train'
+    test_path = './Data/car_plate_test'
 
     # FILE_PATH = './Model/model_params.pkl'
     # model_train = model_cnn_torch.ModuleTrain(train_path, test_path, FILE_PATH, img_size=178, lr=1e-4)
 
     FILE_PATH = './Model/model_resnet18_params.pkl'
     model = model_resnet_torch.ResNet18(num_classes=8)
-    model_train = model_cnn_torch.ModuleTrain(train_path, test_path, FILE_PATH, model=model, batch_size=16, img_size=224, lr=1e-6)
+    model_train = model_cnn_torch.ModuleTrain(train_path, test_path, FILE_PATH, model=model, batch_size=16, img_size=224, lr=1e-5)
 
     # FILE_PATH = './Model/model_resnet34_params.pkl'
     # model = models.resnet34(num_classes=8)
@@ -22,5 +22,5 @@ if __name__ == '__main__':
     # model = models.resnet50(num_classes=8)
     # model_train = model_cnn_torch.ModuleTrain(train_path, test_path, FILE_PATH, model=model, batch_size=16, img_size=224, lr=1e-3)
 
-    model_train.train(30)
+    model_train.train(50)
     model_train.test(show_img=True)
