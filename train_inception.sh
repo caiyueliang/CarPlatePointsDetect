9ff3c43fbb0bf5aafbccbd2e3be7fe2fd62f5f3c
lijinchao2007 <lijinchao2007@163.com>
2018-11-27 17:13:19 +0800

    update

--- train_inception.sh
+++ train_inception.sh
@@ -1,4 +1,4 @@
-python detect_car_point.py --model_name inception_v3 --img_size 299 --batch_size 4
+python detect_car_point.py --model_name inception_v3 --img_size 299 --batch_size 16
 python detect_car_point.py --model_name inception_v3 --img_size 299 --batch_size 16
 python detect_car_point.py --model_name inception_v3 --img_size 299 --batch_size 16
 
