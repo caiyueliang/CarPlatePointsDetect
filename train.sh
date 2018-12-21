7e2b6a983a5b5b6d67aeef1bd07ed40b20d6cff9
lijinchao2007 <lijinchao2007@163.com>
2018-12-21 16:48:31 +0800

    update

--- train.sh
+++ train.sh
@@ -1,3 +1,2 @@
-python detect_car_point.py --lr 0.001
-python detect_car_point.py --lr 0.001
-python detect_car_point.py --lr 0.001
+python detect_car_point.py --lr 0.0001
+python detect_car_point.py --lr 0.00001
